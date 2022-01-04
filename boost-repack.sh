fe08da6a2a90c349ad2128adeaceaec5790f6c62
Antonio Coratelli <antoniocoratelli@gmail.com>
2022-01-05 00:07:36 +0100

    Setup

---
+++ boost-repack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+git clone \
+    --jobs $(nproc) \
+    --shallow-submodules \
+    --recurse-submodules \
+    --single-branch \
+    --branch "$1" \
+    https://github.com/boostorg/boost && \
+(cd boost && \
+    git ls-files --recurse-submodules \
+    | grep -Ev '^"?libs/wave/' \
+    | tar cavf "../$1.tgz" -T-)
